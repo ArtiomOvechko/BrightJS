7858f6d8c53ab5ef58e141acf761b2f6726a6477
Artiom Ovechko <ovechko.a@dbbest.com>
2017-11-10 14:41:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Add following lones to your code (change href and src attributes depending on file location)
+
+<link rel="stylesheet" type="text/css" href="bright.css" />
+<script type="text/javascript" src="bright.js"></script>
+
+Add <div class="bright"></div> to your .html file or just add the class to needed div
+
+Invoke Bright.start() function (you can specify delay between changes in milliseconds as parameter)
+Invoke Bright.stop() function to freeze animation
---
+++ bright.js
@@ -0,0 +1,58 @@
+var Bright = function() {
+    var delay;
+    var inProgress = false;
+    var brandName = "bright";
+
+    function process() {
+        foreach(document.querySelectorAll('.' + brandName), (element) => {
+            generateBrightContent(element);
+        });
+        setTimeout(inProgress ? process : () => {}, delay);
+    }
+
+    function generateBrightContent(bright) {
+        var newLayout = "";
+        for (var i = 0, j = getRandomNumber() * 10; i < j; i++) {
+            newLayout += '<div class="' + getRandomColorClass() + '" style="top: ' + getRandomNumber() * 10 + 'px; left: ' + getRandomNumber() * 10 + 'px; width: ' + getRandomNumber() * 10 + '%; height: ' + getRandomNumber() * 10 + '%; "></div>'
+        }
+        bright.innerHTML = newLayout;
+    }
+
+    function start(newDelay) {
+        delay = newDelay;
+        inProgress = true;
+        process();
+    }
+
+    function stop() {
+        inProgress = false;
+        foreach(document.querySelectorAll('.' + brandName), (element) => {
+            element.innerHTML = '';
+        });
+    }
+
+    function getRandomColorClass() {
+        switch(Math.floor(getRandomNumber()/3)) {
+            case 1: return brandName + '-red';
+            case 2: return brandName + '-green';
+            case 3: default:  return brandName + '-blue';
+        }
+    }
+
+    function foreach(collection, processor) {
+        for (var i = 0, j = collection.length; i < j; i++) {
+            processor(collection[i]);
+        }
+    }
+
+    function getRandomNumber() {
+        return Math.floor((Math.random() * 10) + 1); 
+    }
+
+    return {
+        start: start,
+        stop: stop,
+
+        generateBrightContent: generateBrightContent
+    }
+}();
