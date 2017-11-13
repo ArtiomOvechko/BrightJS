66f60026d843e8dcd7474ae3c99ece0037fd8807
Artiom Ovechko <ovechko.a@dbbest.com>
2017-11-13 18:02:20 +0200

    Flexible bright added; removed .css file

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
-Add following lones to your code (change href and src attributes depending on file location)
+<div><b>Flexible Bright.js is now in development!</b><div>
+<div>
+To style all the HTML on any site add following lones to your code (change href and src attributes depending on file location)
 
  ```
-<link rel="stylesheet" type="text/css" href="bright.css" />
 <script type="text/javascript" src="bright.js"></script>
  ```
  
 Add  ```<code><div class="bright"></div><code> ``` to your .html file or just add the class to needed div
 
-Invoke Bright.start() function (you can specify delay between changes in milliseconds as parameter)
-Invoke Bright.stop() function to freeze animation
+Invoke ```Bright.init()``` function to apply styles to page
+Invoke ```Bright.epilepsify()``` function
+Invoke ```Bright.stop()``` function to freeze animation
+</div>
+
+<div><b>Strict Bright.js</b><div>
+<div>
+To generate random bright divs inside targets add following lones to your code (change href and src attributes depending on file location)
+
+ ```
+<script type="text/javascript" src="bright.js"></script>
+ ```
+ 
+Add  ```<code><div class="bright"></div><code> ``` to your .html file or just add the class to needed div
+
+Invoke ```Bright.init()``` function to apply styles to page
+Invoke ```Bright.start()``` function (you can specify delay between changes in milliseconds as parameter)
+Invoke ```Bright.stop()``` function to freeze animation
+</div>
--- bright.js
+++ bright.js
@@ -1,7 +1,97 @@
 var Bright = function() {
     var delay;
+    var epilepsyDelay;
     var inProgress = false;
     var brandName = "bright";
+    var inEpilepsy = false;
+
+    var customStyles = 
+    `.bright {
+        width: 100% !important;
+        height: 100% !important;
+    } .bright div {
+        position: absolute !important;
+    }     
+
+    .bright-bg-color-first {
+        background-color: pink;
+        animation: bg-first 0.1s infinite;
+        -webkit-animation: bg-first 0.1s infinite;
+      }
+      
+      @-webkit-keyframes bg-first {
+        0% { background-color: pink; }
+        20% { background-color: yellow; }
+        40% { background-color: red; }
+        60% { background-color: cyan; }
+        80% { background-color: greenyellow; }
+        100% { background-color: orange; }
+      }
+      
+      @keyframes bg-first {
+        0% { background-color: pink; }
+        20% { background-color: yellow; }
+        40% { background-color: red; }
+        60% { background-color: cyan; }
+        80% { background-color: greenyellow; }
+        100% { background-color: orange; }
+      }
+
+      .bright-bg-color-second {
+        background-color: cyan;
+        animation: bg-second 0.1s infinite;
+        -webkit-animation: bg-second 0.1s infinite;
+      }
+      
+      @-webkit-keyframes bg-second {
+        0% { background-color: cyan; }
+        20% { background-color: pink; }
+        40% { background-color: greenyellow; }
+        60% { background-color: orange; }
+        80% { background-color: red; }
+        100% { background-color: yellow; }
+      }
+      
+      @keyframes bg-second {
+        0% { background-color: cyan; }
+        20% { background-color: pink; }
+        40% { background-color: greenyellow; }
+        60% { background-color: orange; }
+        80% { background-color: red; }
+        100% { background-color: yellow; }
+      }
+
+      .bright-bg-color-third {
+        background-color: red;
+        animation: bg-third 0.1s infinite;
+        -webkit-animation: bg-third 0.1s infinite;
+      }
+      
+      @-webkit-keyframes bg-third {
+        0% { background-color: red; }
+        20% { background-color: pink; }
+        40% { background-color: cyan; }
+        60% { background-color: greenyellow; }
+        80% { background-color: orange; }
+        100% { background-color: yellow; }
+      }
+      
+      @keyframes bg-third {
+        0% { background-color: red; }
+        20% { background-color: pink; }
+        40% { background-color: cyan; }
+        60% { background-color: greenyellow; }
+        80% { background-color: orange; }
+        100% { background-color: yellow; }
+      }
+    `;
+
+    function init() {
+        var style = document.createElement('style');
+        style.setAttribute('type', 'text/css');
+        style.innerHTML = customStyles;
+        document.body.appendChild(style);
+    };
 
     function process() {
         foreach(document.querySelectorAll('.' + brandName), (element) => {
@@ -13,9 +103,32 @@ var Bright = function() {
     function generateBrightContent(bright) {
         var newLayout = "";
         for (var i = 0, j = getRandomNumber() * 10; i < j; i++) {
-            newLayout += '<div class="' + getRandomColorClass() + '" style="top: ' + getRandomNumber() * 10 + 'px; left: ' + getRandomNumber() * 10 + 'px; width: ' + getRandomNumber() * 10 + '%; height: ' + getRandomNumber() * 10 + '%; "></div>'
+            newLayout += '<div class="' + getRandomColorClass('-bg') + '" style="top: ' + getRandomNumber() * 10 + 'px; left: ' + getRandomNumber() * 10 + 'px; width: ' + getRandomNumber() * 10 + '%; height: ' + getRandomNumber() * 10 + '%; "></div>'
         }
         bright.innerHTML = newLayout;
+    }
+
+    function epilepsify(newDelay) {
+        epilepsyDelay = newDelay;
+        var elements = document.querySelectorAll('*');
+        inEpilepsy = true;
+        epilepsy(elements)
+        //inEpilepsy ? () => { inEpilepsy = false } : () => { inEpilepsy = true; epilepsy(elements) };
+    }
+
+    function epilepsy(elements) {
+        foreach(elements, (element) => {
+            replaceColorClass(element, '-bg');
+            // replaceColorClass(element);
+        });
+        //setTimeout(inEpilepsy ? () => { epilepsy(elements) } : () => {}, epilepsyDelay);
+    }
+
+    function replaceColorClass(element, modifier) {
+        modifier = modifier || '';
+        try {
+            element.className = element.className.split(' ').filter(cl => cl.indexOf(brandName + modifier + '-color') == -1).join(' ') + ' ' + getRandomColorClass(modifier);
+        } catch(ex) {}
     }
 
     function start(newDelay) {
@@ -31,11 +144,12 @@ var Bright = function() {
         });
     }
 
-    function getRandomColorClass() {
+    function getRandomColorClass(modifier) {
+        modifier = modifier || '';
         switch(Math.floor(getRandomNumber()/3)) {
-            case 1: return brandName + '-red';
-            case 2: return brandName + '-green';
-            case 3: default:  return brandName + '-blue';
+            case 1: return brandName + modifier + '-color-first';
+            case 2: return brandName + modifier + '-color-second';
+            case 3: default:  return brandName + modifier + '-color-third';
         }
     }
 
@@ -50,9 +164,9 @@ var Bright = function() {
     }
 
     return {
+        init: init,
         start: start,
         stop: stop,
-
-        generateBrightContent: generateBrightContent
+        epilepsify: epilepsify
     }
 }();
